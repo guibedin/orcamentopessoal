3c2da8d7b19232b23b647dd8ec1a6b8fe26b4be1
Guilherme Bedin <guilherme0bedin@gmail.com>
2018-10-28 23:58:55 -0300

    Configuration change. Removing application.properties

--- src/main/java/br/com/guibedin/orcamentopessoal/MyConfiguration.java
+++ src/main/java/br/com/guibedin/orcamentopessoal/MyConfiguration.java
@@ -38,7 +38,7 @@ public class MyConfiguration extends WebSecurityConfigurerAdapter {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-					.allowedOrigins("http://localhost:3000", "https://localhost:3000")
+					.allowedOrigins("http://localhost:3000", "https://localhost:3000", "http://guibedin.github.io", "https://guibedin.github.io")
 					.allowedMethods("*");
 			 }
 		};
